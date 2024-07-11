acb869ba1784b45d3dcf933c5817ac25a9a2234b
Laden <delfanladen24@gmail.com>
2024-07-11 23:32:17 +0800

    adding dark mode button in navheads

--- src/components/NavHead.tsx
+++ src/components/NavHead.tsx
@@ -1,6 +1,17 @@
 import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 import { Button } from './ui/button'
-import { Badge, CircleUser, Home, LineChart, Menu, Package, Package2, ShoppingCart, Users } from 'lucide-react'
+import {
+  Badge,
+  Bell,
+  CircleUser,
+  Home,
+  LineChart,
+  Menu,
+  Package,
+  Package2,
+  ShoppingCart,
+  Users,
+} from 'lucide-react'
 import Link from 'next/link'
 import {
   DropdownMenu,
@@ -11,6 +22,7 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import { useAuth } from '@/hooks/auth'
+import { ModeToggle } from './ModeToggle'
 
 const NavHead = () => {
   const { user } = useAuth({ middleware: 'auth' })
@@ -19,19 +31,31 @@ const NavHead = () => {
     <header className="flex h-14 items-center bg-background justify-between md:justify-end gap-4 border-b px-4 lg:h-[60px] lg:px-6">
       <Sheet>
         <SheetTrigger asChild>
-          <Button variant="outline" size="icon" className="text-muted-foreground shrink-0 md:hidden">
+          <Button
+            variant="outline"
+            size="icon"
+            className="text-muted-foreground shrink-0 md:hidden">
             <Menu className="h-5 w-5" />
             <span className="sr-only">Toggle navigation menu</span>
           </Button>
         </SheetTrigger>
         <SheetContent side="left" className="flex flex-col">
           <nav className="grid gap-2 text-lg font-medium">
-            <Link
-              href="#"
-              className="flex text-muted-foreground items-center gap-4 text-gray-900 text-2xl font-bold">
-              <Package2 className="h-6 w-6" />
-              <span className="">Kosthority</span>
-            </Link>
+            <div className="flex h-14 items-center justify-between border-b gap-2 lg:h-[60px] lg:px-6">
+              <Link
+                href="/"
+                className="dark:text-white flex items-center gap-2 font-semibold">
+                <Package2 className="h-6 w-6" />
+                <span>Kosthority</span>
+              </Link>
+              <div className="flex justify-end gap-2">
+                <Button variant="secondary" size="icon" className="h-8 w-8">
+                  <Bell className="h-4 w-4" />
+                  <span className="sr-only">Toggle notifications</span>
+                </Button>
+                <ModeToggle />
+              </div>
+            </div>
             <Link
               href="#"
               className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
