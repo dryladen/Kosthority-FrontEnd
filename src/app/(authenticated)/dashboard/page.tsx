b01a71d3af0d5eb28acb7f4f576afa96fda555cf
Laden <delfanladen24@gmail.com>
2024-07-11 16:20:31 +0800

    create dashboard

--- src/app/(authenticated)/dashboard/page.tsx
+++ src/app/(authenticated)/dashboard/page.tsx
@@ -1,4 +1,6 @@
 'use client'
+import CardDashboard from '@/components/CardDashboard'
+import NavHead from '@/components/NavHead'
 import { useData } from '@/hooks/dataKost'
 import { RentalHouse } from '@/types/RentalHouse'
 import { UserType } from '@/types/User'
@@ -7,6 +9,7 @@ import Link from 'next/link'
 import {
   Bell,
   CircleUser,
+  DollarSign,
   Home,
   LayoutDashboard,
   LineChart,
@@ -15,6 +18,7 @@ import {
   Package2,
   Search,
   ShoppingCart,
+  User,
   Users,
 } from 'lucide-react'
 import { Badge } from '@/components/ui/badge'
@@ -31,6 +35,7 @@ import { Input } from '@/components/ui/input'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
 import { useAuth } from '@/hooks/auth'
 import { DropdownButton } from '@/components/DropdownLink'
+import Transactions from '@/components/Transaction'
 
 const DashboardPage = () => {
   const { user } = useAuth({ middleware: 'auth' })
@@ -92,110 +97,29 @@ const DashboardPage = () => {
       {/* main */}
       <div className="flex flex-col">
         {/* header */}
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden">
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col">
-              <nav className="grid gap-2 text-lg font-medium">
-                <Link
-                  href="#"
-                  className="flex items-center gap-4 text-gray-900 text-2xl font-bold">
-                  <Package2 className="h-6 w-6" />
-                  <span className="">Kosthority</span>
-                </Link>
-                <Link
-                  href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                  <Home className="h-5 w-5" />
-                  Dashboard
-                </Link>
-                <Link
-                  href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
-                  <ShoppingCart className="h-5 w-5" />
-                  Orders
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
-                  </Badge>
-                </Link>
-                <Link
-                  href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                  <Package className="h-5 w-5" />
-                  Products
-                </Link>
-                <Link
-                  href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                  <Users className="h-5 w-5" />
-                  Customers
-                </Link>
-                <Link
-                  href="#"
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
-                  <LineChart className="h-5 w-5" />
-                  Analytics
-                </Link>
-              </nav>
-            </SheetContent>
-          </Sheet>
-          {/* <div className="w-full flex-1">
-            <form>
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search products..."
-                  className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                />
-              </div>
-            </form>
-          </div> */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                variant="secondary"
-                className="rounded-full gap-2 focus-visible:ring-0 focus-visible:ring-offset-0 bg-white hover:bg-white">
-                <span>{user?.name}</span>
-                <CircleUser className="h-5 w-5" />
-                <span className="sr-only">Toggle user menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Settings</DropdownMenuItem>
-              <DropdownMenuItem>Support</DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+        <NavHead />
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6 ">
+          <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+            <CardDashboard
+              title="Income"
+              logo={<DollarSign className="w-4 h-4 text-muted-foreground"/>}
+              content="Rp.15.000"
+              desc=""
+            />
+            <CardDashboard
+              title="Tenants"
+              logo={<Users className="h-4 w-4 text-muted-foreground" />}
+              content="24"
+              desc=""
+            />
+            <CardDashboard
+              title="Income"
+              logo={<DollarSign className="w-5 h-5"></DollarSign>}
+              content="Rp.15.000"
+              desc=""
+            />
           </div>
-          <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-            x-chunk="dashboard-02-chunk-1">
-            <div className="flex flex-col items-center gap-1 text-center">
-              <h3 className="text-2xl font-bold tracking-tight">
-                You have no products
-              </h3>
-              <p className="text-sm text-muted-foreground">
-                You can start selling as soon as you add a product.
-              </p>
-              <Button className="mt-4">Add Product</Button>
-            </div>
-          </div>
+          <Transactions />
         </main>
       </div>
     </div>
---
+++ src/components/NavHead.tsx
@@ -0,0 +1,94 @@
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import { Button } from './ui/button'
+import { Badge, CircleUser, Home, LineChart, Menu, Package, Package2, ShoppingCart, Users } from 'lucide-react'
+import Link from 'next/link'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { useAuth } from '@/hooks/auth'
+
+const NavHead = () => {
+  const { user } = useAuth({ middleware: 'auth' })
+  const { logout } = useAuth({})
+  return (
+    <header className="flex h-14 items-center justify-between md:justify-end gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+            <Menu className="h-5 w-5" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="flex flex-col">
+          <nav className="grid gap-2 text-lg font-medium">
+            <Link
+              href="#"
+              className="flex items-center gap-4 text-gray-900 text-2xl font-bold">
+              <Package2 className="h-6 w-6" />
+              <span className="">Kosthority</span>
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+              <Home className="h-5 w-5" />
+              Dashboard
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground">
+              <ShoppingCart className="h-5 w-5" />
+              Orders
+              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+                6
+              </Badge>
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+              <Package className="h-5 w-5" />
+              Products
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+              <Users className="h-5 w-5" />
+              Customers
+            </Link>
+            <Link
+              href="#"
+              className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground">
+              <LineChart className="h-5 w-5" />
+              Analytics
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="secondary"
+            className="rounded-full gap-2 focus-visible:ring-0 focus-visible:ring-offset-0 bg-white hover:bg-white">
+            <span>{user?.name}</span>
+            <CircleUser className="h-5 w-5" />
+            <span className="sr-only">Toggle user menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={logout}>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  )
+}
+
+export default NavHead
